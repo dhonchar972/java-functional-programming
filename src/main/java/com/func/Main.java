924b50b1d6d748e924b746a799d56ed71b9ff5b5
Dmytro Honchar <dmytro.honchar972@gmail.com>
2022-09-06 13:33:35 +0300

    init commit

---
+++ src/main/java/com/func/Main.java
@@ -0,0 +1,11 @@
+package com.func;
+
+/**
+ * Created by Dmytro Honchar
+ * Date: 9/6/2022
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("BOY!!!");
+    }
+}
