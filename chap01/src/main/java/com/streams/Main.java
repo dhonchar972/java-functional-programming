b5b0d644968d7a7be5c47d1a14fc04d0f01df5d5
Dmytro Honchar <dmytro.honchar972@gmail.com>
2022-11-06 21:12:56 +0200

    updated project

--- chap01/src/main/java/com/streams/Main.java
+++ chap01/src/main/java/com/streams/Main.java
@@ -138,18 +138,7 @@ public class Main {
     }
 }
 
-class Dish {
-    private final String name;
-    private final boolean vegetarian;
-    private final int calories;
-    private final Type type;
-
-    public Dish(String name, boolean vegetarian, int calories, Type type) {
-        this.name = name;
-        this.vegetarian = vegetarian;
-        this.calories = calories;
-        this.type = type;
-    }
+record Dish(String name, boolean vegetarian, int calories, com.streams.Dish.Type type) {
 
     public String getName() {
         return name;
